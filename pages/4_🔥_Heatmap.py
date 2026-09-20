f818b6d378d9970f0eeff41a6223c88a78cf1811
ChazzC <102876843+ChazzC@users.noreply.github.com>
2026-09-20 15:56:40 -0500

    Update 4_🔥_Heatmap.py
    testing tiler without leafmap

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -1,99 +1,58 @@
-import numpy as np
-import json
 import streamlit as st
-import leafmap.foliumap as leafmap
 import requests
 
 st.set_page_config(layout="wide")
 
-st.sidebar.info("""
-    - Web App URL: <https://streamlit.gishub.org>
-    - GitHub repository: https://github.com/opengeos/streamlit-geospatial
-""")
+st.title("TiTiler COG Test")
 
-st.sidebar.title("Contact")
-st.sidebar.info("""
-    # Chazz Coleman at |
-    [GitHub](https://github.com/ChazzC) |
-    # [Twitter](https://twitter.com/giswqs) |
-    # [YouTube](https://youtube.com/@giswqs) |
-    [LinkedIn](https://www.linkedin.com/in/chazz-coleman)
-""")
+dem_filepath = (
+    "https://raw.githubusercontent.com/"
+    "ChazzC/Fishes-Of-The-MS-Sound-And-Barrier-Islands/"
+    "main/data/Elevation_and_Bathymertry_Study_Area.tiff.tif"
+)
 
-st.title("Heatmap")
+st.write("Testing TiTiler...")
 
-with st.expander("See source code"):
-    with st.echo():
+try:
 
-        filepath = (
-            "https://raw.githubusercontent.com/"
-            "giswqs/leafmap/master/examples/data/us_cities.csv"
+    titiler_url = "https://titiler.opengeos.org/cog/preview"
+
+    params = {
+        "url": dem_filepath,
+        "bidx": 1,
+        "rescale": "-19.212,57.122",
+        "colormap_name": "terrain",
+    }
+
+    response = requests.get(
+        titiler_url,
+        params=params,
+        timeout=30,
+    )
+
+    st.write("TiTiler HTTP status:", response.status_code)
+
+    st.write("Response type:", response.headers.get("content-type"))
+
+    if response.ok:
+
+        st.image(
+            response.content,
+            caption="TiTiler terrain test",
+            use_container_width=True,
         )
 
-        dem_filepath = (
-            "https://raw.githubusercontent.com/"
-            "ChazzC/Fishes-Of-The-MS-Sound-And-Barrier-Islands/"
-            "main/data/Elevation_and_Bathymertry_Study_Area.tiff.tif"
-        )
-                # Test TiTiler directly
-        titiler_url = "https://titiler.opengeos.org/cog/preview"
-        
-        params = {
-            "url": dem_filepath,
-            "bidx": 1,
-            "rescale": "-19.212,57.122",
-            "colormap_name": "viridis",
-            "return_mask": True,
-        }
-        
-        response = requests.get(titiler_url, params=params)
-        
-        st.write("TiTiler status:", response.status_code)
-        
-        if response.ok:
-            st.image(
-                response.content,
-                caption="Direct TiTiler test",
-                use_container_width=True,
-            )
-        else:
-            st.error("TiTiler returned an error:")
-            st.code(response.text)
+    else:
 
-        # m = leafmap.Map(center=[31, -88], zoom=8)
-        
-          
-        # custom_colormap = json.dumps({
-        #     "0":   "#08306B",
-        #     "14":  "#08519C",
-        #     "31":  "#2171B5",
-        #     "48":  "#41B6C4",
-        #     "64":  "#00A65A",
-        #     "71":  "#7FCF3F",
-        #     "81":  "#D9EF3D",
-        #     "98":  "#FEE08B",
-        #     "131": "#F46D43",
-        #     "181": "#D73027",
-        #     "255": "#7F3B08",
-        # })  
+        st.error("TiTiler returned an error.")
 
-        # Add the Cloud Optimized GeoTIFF
-       # Add the COG
-        # m.add_cog_layer(
-        #     dem_filepath,
-        #     name="Elevation & Bathymetry",
-        #     bands=[1],
-        #     rescale="-19.212,57.122",
-        #     colormap=custom_colormap,
-        # )         
+        st.code(response.text)
 
-#         m.add_heatmap(
-#             filepath,
-#             latitude="latitude",
-#             longitude="longitude",
-#             value="pop_max",
-#             name="Heat map",
-#             radius=20,
-#         )
+except requests.exceptions.Timeout:
 
-# m.to_streamlit(height=700)
+    st.error("TiTiler request timed out after 30 seconds.")
+
+except Exception as e:
+
+    st.error("Something went wrong:")
+    st.exception(e)
