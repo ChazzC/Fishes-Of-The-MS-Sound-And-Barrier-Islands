c4de2ab61aec06217e78cb35a8c6a4b6b9041158
ChazzC <102876843+ChazzC@users.noreply.github.com>
2026-09-20 13:33:47 -0500

    Update 4_🔥_Heatmap.py
    changed url path for dem raster

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -19,7 +19,7 @@ with st.expander("See source code"):
     with st.echo():
         filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
         dem_filepath = "https://github.com/ChazzC/Fishes-Of-The-MS-Sound-And-Barrier-Islands/blob/main/data/Elevation_and_Bathymertry_Study_Area.tiff.tif"
-        m = leafmap.Map(center=[40, -100], zoom=4)
+        m = leafmap.Map(center=[31, -88], zoom=4)
         m.add_raster(dem_filepath, colormap="terrain", layer_name="DEM")
         m.add_heatmap(
             filepath,
