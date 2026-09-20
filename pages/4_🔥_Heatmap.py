87b859a78efeaa4211bba47300d3f2d308c8b889
ChazzC <102876843+ChazzC@users.noreply.github.com>
2026-09-20 12:51:38 -0500

    Update 4_🔥_Heatmap.py
    added MS Sound DEM

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -18,7 +18,9 @@ st.title("Heatmap")
 with st.expander("See source code"):
     with st.echo():
         filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+        dem_filepath = "https://github.com/ChazzC/Fishes-Of-The-MS-Sound-And-Barrier-Islands/blob/main/data/Elevation_and_Bathymertry_Study_Area.tiff.tif"
         m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_raster(dem_filepath, colormap="terrain", layer_name="DEM")
         m.add_heatmap(
             filepath,
             latitude="latitude",
