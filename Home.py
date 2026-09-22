0e8e823b3d8797417eb0c04bbb3b048264b8fa58
ChazzC <102876843+ChazzC@users.noreply.github.com>
2026-09-21 23:05:41 -0500

    Update Home.py
    added nautical chart screenshot

--- Home.py
+++ Home.py
@@ -42,7 +42,7 @@ st.markdown("""
 
 row1_col1, row1_col2 = st.columns(2)
 with row1_col1:
-    st.image("https://github.com/giswqs/data/raw/main/timelapse/spain.gif")
+    st.image("photos/37F0A380-8729-4B91-8E7B-56CF94ECA801.png")
     st.image("https://github.com/giswqs/data/raw/main/timelapse/las_vegas.gif")
 
 with row1_col2:
