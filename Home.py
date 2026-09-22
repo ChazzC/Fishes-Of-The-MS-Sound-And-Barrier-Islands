fb61acec8e07f14ab9bff26a9446c6939d1c2664
ChazzC <102876843+ChazzC@users.noreply.github.com>
2026-09-21 22:22:24 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,12 +25,12 @@ st.title("Fishes of the Mississippi Sound and Barrier Islands")
 
 st.markdown("""
     This multi-page web app demonstrates an interactive web app created using [streamlit](https://streamlit.io) and open-source mapping libraries,
-    such as [leafmap](https://leafmap.org). The fish records/occurences were compiled from multiple sources and span decades. Source attributions are within the fish point layer.
-    Institution collections on GBIF: Mississippi Museum of Natural Science, University of Alabama
-    Institution collections that were self hosted: University of Southern Mississippi Ichthyology Collection
-    SEAMAP Bottom Long Line survey data
-    SEAMAP Groundfish survey data
-    Research Grade iNaturalist observations
+    such as [leafmap](https://leafmap.org). The fish records/occurences were compiled from multiple sources and span decades. Source attributions are within the fish point layer and also listed here.
+    Institution collections on GBIF: Mississippi Museum of Natural Science & University of Alabama.
+    Institution collections that were self hosted: University of Southern Mississippi Ichthyology Collection.
+    SEAMAP Bottom Long Line survey data.
+    SEAMAP Groundfish survey data.
+    Research Grade iNaturalist observations.
     """)
 
 st.info("Click on the left sidebar menu to navigate to the different apps.")
