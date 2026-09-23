ea5a103afe87f49d74ba3d3dd30b1ee8fc6189e2
ChazzC <102876843+ChazzC@users.noreply.github.com>
2026-09-23 15:18:01 -0500

    Update Home.py
    added links for fish records/occurrence data

--- Home.py
+++ Home.py
@@ -26,11 +26,10 @@ st.title("Fishes of the Mississippi Sound and Barrier Islands - From Lake Borgne
 st.markdown("""
     This multi-page web app demonstrates an interactive web app created using [streamlit](https://streamlit.io) and open-source mapping libraries,
     such as [leafmap](https://leafmap.org). The fish records/occurences were compiled from multiple sources and span decades. Source attributions are within the fish point layer and also listed here.
-    - Institution collections on GBIF: Mississippi Museum of Natural Science & University of Alabama.
+    - Institution collections on [GBIF](https://www.gbif.org/occurrence/search?occurrenceStatus=present): Mississippi Museum of Natural Science & University of Alabama.
     - Institution collections that were self hosted: [University of Southern Mississippi Ichthyology Collection](https://ichthyology.usm.edu/shiny/map/).
-    - SEAMAP Bottom Long Line survey data.
-    - SEAMAP Groundfish survey data.
-    - Research Grade iNaturalist observations.
+    - [SEAMAP](https://seamapdata.gsmfc.org/pages/download-form.php) Bottom Long Line survey data & Groundfish survey data.
+    - Research Grade [iNaturalist](https://www.inaturalist.org/observations) observations.
     """)
 
 st.info("Click on the left sidebar menu to navigate to the different apps.")
